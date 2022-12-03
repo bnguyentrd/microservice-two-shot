6edf2e22689af6e234434d3666a1c7d1739ce6da
anthony.chang <anthony.chang92@gmail.com>
2022-12-02 19:10:01 -0800

    shoes models/views/urls

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class BinVO(models.Model):
+    closet_name = models.CharField(max_length=150)
+    bin_number = models.PositiveSmallIntegerField()
+    bin_size = models.PositiveSmallIntegerField()
+
+
+
+class Shoes(models.Model):
+    manufacturer = models.CharField(max_length=100)
+    model_name = models.CharField(max_length=100)
+    color = models.CharField(max_length=25)
+    picture_url = models.URLField(null=True)
+    bin = models.ForeignKey(
+        BinVO,
+        related_name="shoes",
+        on_delete=models.CASCADE,
+        null=True
+    )
+
+
+    def get_api_url(self):
+        return reverse("api_show_shoe", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return f'This is a {self.color} {self.manufacturer} in {self.bin}'
--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,93 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 
-# Create your views here.
+from common.json import ModelEncoder
+from .models import BinVO, Shoes
+
+class BinVODetailEncoder(ModelEncoder):
+    model = BinVO
+    properties = [
+        "id",
+        "closet_name",
+        "bin_number",
+        "bin_size",
+    ]
+
+class ShoeListEncoder(ModelEncoder):
+    model = Shoes
+    properties = ["id"]
+
+    def get_extra_data(self, o):
+        return {"bin": o.bin.closet_name}
+
+class ShoeDetailEncoder(ModelEncoder):
+    model = Shoes
+    properties = [
+        "pictured_url",
+        "brand",
+        "color",
+        "bin",
+    ]
+
+@require_http_methods(["GET", "POST"])
+def api_list_shoes(request):
+    if request.method == "GET":
+        shoes = Shoes.objects.all()
+        return JsonResponse(
+            {"shoes": shoes},
+            encoder=ShoeListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        shoes = Shoes.objects.create(**content)
+        return JsonResponse(
+            shoes,
+            encoder=ShoeListEncoder,
+            safe=False,
+        )
+
+@require_http_methods(["GET", "DELETE", "PUT"])
+def api_show_shoes(request, pk):
+    if request.method == "GET":
+        try:
+            shoes = Shoes.objects.get(id=pk)
+            return JsonResponse(
+                shoes,
+                encoder=ShoeDetailEncoder
+            )
+        except Shoes.DoesNotExist:
+            response = JsonResponse({"message": "Does not exist"})
+            response.status_code = 404
+            return response
+    elif request.method == "DELETE":
+        try:
+            shoes = Shoes.objects.get(id=pk)
+            shoes.delete()
+            return JsonResponse(
+                shoes,
+                encoder=ShoeDetailEncoder,
+                safe=False,
+            )
+        except Shoes.DoesNotExist:
+            return JsonResponse({"message": "Does not exist"})
+    else:
+        try:
+            content = json.loads(request.body)
+            shoes = Shoes.objects.get(id=pk)
+
+            props = ["pictured_url", "brand", "color", "bin"]
+            for prop in props:
+                if prop in content:
+                    setattr(shoes, prop, content[prop])
+            shoes.save()
+            return JsonResponse(
+                shoes,
+                encoder=ShoeDetailEncoder,
+                safe=False,
+            )
+        except Shoes.DoesNotExist:
+            response = JsonResponse({"message": "Does not exist"})
+            response.status_code = 404
+            return response
